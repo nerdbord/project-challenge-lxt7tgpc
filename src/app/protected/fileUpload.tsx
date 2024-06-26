3351839c42dcdfb6912bbe08315d4e9524d0dfc9
fwas <fwas.corp@gmail.com>
2024-06-26 19:00:44 +0200

    upload works :)

---
+++ src/app/protected/fileUpload.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+
+interface FileUploadProps{
+    userName: string;
+} 
+const FileUpload = (props: FileUploadProps) => {
+  const [file, setFile] = useState(null);
+  const supabase = createClient();
+
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!file) return;
+
+    const { data, error } = await supabase.storage
+      .from("fotobucket") // Replace 'your-bucket-name' with your actual bucket name
+      .upload(`${props.userName}/${file.name}`, file); // Adjust the path as needed
+
+    if (error) {
+      console.error("Error uploading file:", error);
+    } else {
+      console.log("File uploaded successfully:", data);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} id="fileInput" />
+      <button onClick={handleUpload} disabled={!file}>
+        Upload
+      </button>
+    </div>
+  );
+};
+
+export default FileUpload;

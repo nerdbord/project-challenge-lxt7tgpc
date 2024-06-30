fc7f0c4c1395815cdcdb1708da3d626ba312df12
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-06-30 17:52:14 +0200

    added some tailwind features
    added:
    prettier tailwind plugin
    daisyui - tailwind component library
    setup theme for daisyui - In the beginning, I used theme named "cupcake"
    tailwindcss-signals - its veru usefull in specyfic causes
    tailwindcss/typography - use classname prose if you need fast styling blocks of text with many html tags

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,8 +1,8 @@
-import AuthButton from "@/components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import FileUpload from "./fileUpload";
-import Gallery from "./gallery";
+import AuthButton from '@/components/AuthButton';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+import FileUpload from './fileUpload';
+import Gallery from './gallery';
 
 export default async function GalleryPage() {
   const supabase = createClient();
@@ -12,27 +12,28 @@ export default async function GalleryPage() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/login");
+    return redirect('/login');
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <div className="flex w-full flex-1 flex-col items-center gap-20">
       <div className="w-full">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+        <nav className="border-b-foreground/10 flex h-16 w-full justify-center border-b">
+          <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
+            <div>LOGO</div>
             <AuthButton />
           </div>
         </nav>
       </div>
 
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <FileUpload userName={user.email || "public"} />
-        <Gallery userID= {user.id}/>
+      <div className="flex max-w-4xl flex-1 flex-col gap-20 px-3">
+        <FileUpload userName={user.email || 'public'} />
+        <Gallery userID={user.id} />
       </div>
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+      <footer className="border-t-foreground/10 flex w-full justify-center border-t p-8 text-center text-xs">
         <p>
-          Powered by{" "}
+          Powered by{' '}
           <a
             href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
             target="_blank"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Toaster } from 'react-hot-toast'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Toaster } from 'react-hot-toast';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+  : 'http://localhost:3000';
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'Gallery',
   description: 'Personal Gallery with public URLs',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main className="min-h-screen flex flex-col items-center">
-          <Toaster position='bottom-right' />
+      <body className={`${inter.className} bg-none`} data-theme="cupcake">
+        <main className="flex min-h-screen w-full flex-col items-center">
+          <Toaster position="bottom-right" />
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
